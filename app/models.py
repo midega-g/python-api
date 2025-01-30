dd2286522cd4630f63b32fd986b3aa461bf02735
midega-g <midegageorge2@gmail.com>
2025-01-30 15:19:07 +0300

    added foreign keys, relationships, and updated schema

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String, Boolean, Integer
+from sqlalchemy import Column, String, Boolean, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 
@@ -15,6 +16,10 @@ class Post(Base):
     created_at = Column(
         TIMESTAMP(timezone=True), nullable=False, server_default=text('now()')
     )
+    owner_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+    owner = relationship("User")
 
 
 class User(Base):
@@ -25,4 +30,15 @@ class User(Base):
     password = Column(String, nullable=False)
     created_at = Column(
         TIMESTAMP(timezone=True), nullable=False, server_default=text('now()')
+    )
+
+
+class Vote(Base):
+    __tablename__ = "votes"
+
+    post_id = Column(
+        Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True
+    )
+    user_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True
     )
