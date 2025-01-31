e686813b54d0f4d838fddef2cb0c14cdb28519cd
midega-g <midegageorge2@gmail.com>
2025-01-31 18:48:50 +0300

    added test sql queries

---
+++ test-codes/fastapi-test-queries.sql
@@ -0,0 +1,20 @@
+select * from posts; -- 3, 2
+select * from users; -- 1 & 4
+select * from votes;
+
+insert into votes (post_id, user_id) values (4, 3);
+delete from votes;
+
+select posts.*, count(votes.post_id) as likes from posts left join votes on posts.id = votes.post_id group by posts.id;
+
+SELECT users.id, users.email, COUNT(posts.id) AS post_count
+FROM users
+LEFT JOIN posts
+ON users.id = posts.owner_id
+GROUP BY users.id;
+
+SELECT posts.*, COUNT(votes.post_id) AS vote_count
+FROM posts
+LEFT JOIN votes
+ON posts.id = votes.post_id
+GROUP BY posts.id;
