a546cdccf5fb5416abcc60f3759c8d4a292d8737
midega-g <midegageorge2@gmail.com>
2025-02-03 12:41:41 +0300

    optimized for pydantic v2

--- app/config.py
+++ app/config.py
@@ -1,4 +1,7 @@
-from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
@@ -11,8 +14,9 @@ class Settings(BaseSettings):
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8"
+    )
 
 
 settings = Settings()
